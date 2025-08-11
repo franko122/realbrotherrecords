e05bc42346c320d33b02ab3d1a7444fb235e0941
unknown <franko122>
2025-08-10 17:28:06 -0700

    done

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
 function newStart( ) {
-    
+     let one = document.getElementById("one");
+    let two = document.getElementById('two');
+    let toper = document.getElementById('toper')
+
+    one.addEventListener("click",()=>{
+     toper.style.width="100%"
+    })
+
+     two.addEventListener("click",()=>{
+     toper.style.width="0%";
+     toper.style.overflow="hidden"
+    })
 }
-newStart( )
+newStart( );
